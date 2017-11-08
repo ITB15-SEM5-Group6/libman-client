611dc57a3a958b845224fee6b4dc2a1e4da0b4d1
i7Gamer <timorzipa@gmail.com>
2017-11-08 17:06:20 +0100

    Repaired search

--- src/main/java/at/fhv/itb/sem5/team6/libman/client/ClientTest.java
+++ src/main/java/at/fhv/itb/sem5/team6/libman/client/ClientTest.java
@@ -1,6 +1,5 @@
 package at.fhv.itb.sem5.team6.libman.client;
 
-import at.fhv.itb.sem5.team6.libman.shared.enums.Availability;
 import at.fhv.itb.sem5.team6.libman.shared.interfaces.ILibrary;
 import at.fhv.itb.sem5.team6.libman.shared.interfaces.ILibraryFactory;
 
@@ -23,12 +22,12 @@ public class ClientTest {
         ILibraryFactory stubFactory = (ILibraryFactory) registry.lookup("LibraryFactory");
         ILibrary stub = stubFactory.create();
 
-        stub.findAllMedia().forEach(System.out::println);
+        //stub.findAllMedia().forEach(System.out::println);
 
 
-        stub.findAllPhysicalMedia().forEach(System.out::println);
+        //stub.findAllPhysicalMedia().forEach(System.out::println);
 
-        stub.findAllMedia(Availability.AVAILABLE).forEach(System.out::println);
+        //stub.findAllMedia(Availability.AVAILABLE).forEach(System.out::println);
         //stub.findAllMedia(MediaType.BOOK).forEach(System.out::println);
         //stub.findAllMedia("feu").forEach(System.out::println);
         
